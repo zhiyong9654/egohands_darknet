a53a9a38f39d7ea148467e3df2cbcc278417e460
Zhi Yong <zhiyong9654@gmail.com>
2020-02-09 23:48:16 +0800

    Add argparse to script

--- generate_train_test.py
+++ generate_train_test.py
@@ -1,8 +1,9 @@
 #! /bin/python3
 import random
 from pathlib import Path
+import argparse
 
-def generate_train_test(rel_path_to_img_from_darknet, path_to_processed, seed=False, train_ratio=0.7):
+def generate_train_test(rel_path_to_img_from_darknet, seed, train_ratio=0.7):
     """ Function to generate 2 txt files, each line containing the relative
         path to an image from darknet. E.g. data/obj/img1.jpg.
 
@@ -16,6 +17,7 @@ def generate_train_test(rel_path_to_img_from_darknet, path_to_processed, seed=Fa
         Returns:
             None
     """
+    path_to_processed = Path(rel_path_to_img_from_darknet).stem
     if seed:
         random.seed(seed)
     img_paths = list(Path(path_to_processed).glob('*.jpg')) 
@@ -28,5 +30,13 @@ def generate_train_test(rel_path_to_img_from_darknet, path_to_processed, seed=Fa
     with open('test.txt', 'w') as f:
         f.write('\n'.join(test_paths))
 
-generate_train_test('../code_base/egohands_prepared/', 'egohands_prepared')
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Egohands preparation script for Darknet.')
+    parser.add_argument('rel_path', type=str,
+            help='Relative path to egohands_prepared from darknet executable.')
+    parser.add_argument('--seed', type=int, default=False,
+            help='Set seed to a desired integer if you want train test splits to be repeatable')
+    parser.add_argument('--train_ratio', type=float, default=0.7,
+            help='Set percentage of training data here. Defaults to 0.7.')
+    args = parser.parse_args()
+    generate_train_test(args.rel_path, args.seed, args.train_ratio)
